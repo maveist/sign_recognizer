c972248675c3dc6dbf2051432ee622b01263e9f3
Bastien Potiron <bpo@makina-corpus.com>
2020-12-08 14:40:10 +0100

    Remove unused code

--- main.py
+++ main.py
@@ -12,40 +12,6 @@ from model.trainers.trainer import train_model_from_videos
 # TODO donnee entrainement simple "bonjour", "oui", "non", "arbre", "bebe"
 
 
-def run_simple():
-    mp_hands = mp.solutions.hands
-    mp_pose = mp.solutions.pose
-    mp_drawing = mp.solutions.drawing_utils
-    hands = mp_hands.Hands(
-        min_detection_confidence=0.7, min_tracking_confidence=0.5)
-    pose = mp_pose.Pose(min_detection_confidence=0.7, min_tracking_confidence=0.5)
-    cap = cv2.VideoCapture(0)
-    while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-            break
-        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
-        results = hands.process(image)
-        results_pose = pose.process(image)
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        if results.multi_hand_landmarks:
-            print(results.multi_hand_landmarks)
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-        if results_pose.pose_landmarks:
-            mp_drawing.draw_landmarks(
-                image, results_pose.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-        cv2.imshow('MediaPipe Hands', image)
-        if cv2.waitKey(5) & 0xFF == ord('q'):
-            break
-    hands.close()
-    pose.close()
-    cap.release()
-
-
 def run():
     pass
 
