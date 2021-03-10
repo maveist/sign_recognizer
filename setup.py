cf19fd6da72e529f6bd09be2406f98dfb37b9e88
maveist <maveist44@gmail.com>
2021-03-10 21:45:42 +0100

    update setup py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(name='sign_recognizer',
       description='Python side-project which recognize sign (French Sign Language) with mediapipe and tensorflow',
       author='Dahmani Selim',
       url="https://github.com/maveist/sign_recognizer",
+      packages=find_packages("sign_recognizer"),
       version='0.0.1',
       python_requires='>=3.*',
       install_requires=[req.replace("\n", "") for req in open('requirements.txt')]
